e60985497acbff5f57fea8713d9565b0db010f81
Shiwine K Sebastian <98482513+Shiwine@users.noreply.github.com>
2023-07-22 07:52:53 +0530

    Update string_alternate.py

--- Chapter1/string_alternate.py
+++ Chapter1/string_alternate.py
@@ -1,14 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1, word2):
-       k= len(word1)
-       l= len(word2)
        i=j=0
        d=''
-       while i<k or j<l:
-         if i<k:
+       while i<len(word1) or j<len(word2):
+         if i<len(word1):
             d+=word1[i]
             i=i+1
-         if j<l:
+         if j<len(word2):
             d+=word2[j]
             j=j+1
        return d
